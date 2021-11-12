9ba79b2c9603e75f192ec70ecc875ea018e12941
cratas <petr.kratochvil.st1@vsb.cz>
2021-11-12 14:35:59 +0100

    code made readable and comments added

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-var isMapShown;
+var isMapShown; //boolean variable checking if map is shown or not
 
+
+
+// function started when DOM is loaded
 $(document).ready(function() {
 
-    var isMapShown = false;
-    var today = new Date();
+    var isMapShown = false; 
+    var today = new Date(); //getting actual date and time
     var date = today.getDate();
     var time = ('0' + today.getHours()).substr(-2) + ":" + ('0' + today.getMinutes()).substr(-2);
     var dateTime = time + ' - ' + (today.toLocaleString('en-us', { weekday: 'long' })) + ', ' + date +
         ' ' + (today.toLocaleString('en-us', { month: 'long' }).substring(0, 3));
 
-    $('#dateTime').html(dateTime);
+    $('#dateTime').html(dateTime);  //sending date and time into html structure
 
-    $('#submitButton').click(function() {
+    //listener on submit button, get data via api and sends it into html structure
+    $('#submitButton').click(function() {   
         let inputCity = $('#cityInput').val();
         let weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + inputCity + '&appid=f1a2685da05fe3b5355e7f7a70db4d4c&lang=cz';
 
+        //fetching data from API
         fetch(weatherURL)
             .then(response => response.json())
             .then(data => {
-
                 let mapURL = 'https://maps.google.com/maps?q=' + inputCity + '&t=&z=13&ie=UTF8&iwloc=&output=embed';
 
                 $('#mapIFrame').attr("src", mapURL);
@@ -31,12 +35,9 @@ $(document).ready(function() {
                 $("#cloudiness").html(data["clouds"]["all"] + " %");
                 $("#feelTemp").html(parseInt(data["main"]["feels_like"]) - 273 + "°");
                 $("#pressure").html(data["main"]["pressure"] + " hPa");
-
                 $('#cityInput').val("");
-
-
-                console.log(data);
             })
+            //catching error on wrong input (city name)
             .catch(err => {
                 alert("Wrong city name")
                 $("#city").html("");
@@ -46,25 +47,23 @@ $(document).ready(function() {
             });
     });
 
-
+    //listener on button which shows map with entered city
     $('#showMapButton').click(function() {
-        isMapShown = (isMapShown == true) ? isMapShown = false : isMapShown = true;
-        console.log(isMapShown);
-        if (!isMapShown) {
+        isMapShown = (isMapShown == true) ? isMapShown = false : isMapShown = true; //change boolean value on every click on button
+
+        //changing text inside button and create animation on map
+        if (!isMapShown) {  
             $('#showMapButton').text('\u21e3 Mapa');
             $('.map-wrapper').removeClass('animation-slide-down-class');
             $('.map-wrapper').addClass('animation-slide-up-class');
-            // $('.map-wrapper').hide();
             setTimeout(function() {
                 $('.map-wrapper').hide();
-                // window.scrollTo(0, 0);
             }, 1000);
         } else {
             $('#showMapButton').text('\u21E1 Mapa');
             $('.map-wrapper').removeClass('animation-slide-up-class');
             $('.map-wrapper').addClass('animation-slide-down-class');
             $('.map-wrapper').show();
-
 
             for (let i = 0; i < 1000; i++) {
                 setTimeout(function() {
@@ -74,9 +73,9 @@ $(document).ready(function() {
         }
     });
 
+    //listener on buttons with static city names
     $('.fav-cities-button').click(function() {
         let cityName = $(this).text();
-        console.log(cityName);
 
         $('#cityInput').val(cityName);
         $('#submitButton').click();
@@ -85,8 +84,8 @@ $(document).ready(function() {
 
 });
 
+//function making theme via condition id from API
 function checkWeatherCondition(conditionParameter) {
-
     if (conditionParameter >= 200 && conditionParameter < 240) {
         $('.weather-icon').hide();
         $("#iconStorm").show();
@@ -106,7 +105,6 @@ function checkWeatherCondition(conditionParameter) {
         $('.weather-icon').hide();
         $("#iconFog").show();
         changeThemeFog();
-        console.log("kokot");
     } else if (conditionParameter == 800) {
         $('.weather-icon').hide();
         $("#iconSun").show();
@@ -115,13 +113,14 @@ function checkWeatherCondition(conditionParameter) {
         $('.weather-icon').hide();
         $("#iconCloud").show();
         changeThemeCloud();
-
     }
 }
 
+//functions changing theme images and font colours
 function changeThemeRain() {
     $('.conent-wrapper').css("background-image", "url('graphics/rain_background.jpg')");
     $('.all-background').css("background-image", "url('graphics/rain_background_blur.jpg')");
+
     document.documentElement.style
         .setProperty('--lightGrey', '#ababab');
 }
@@ -129,6 +128,7 @@ function changeThemeRain() {
 function changeThemeFog() {
     $('.conent-wrapper').css("background-image", "url('graphics/fog_background.jpg')");
     $('.all-background').css("background-image", "url('graphics/fog_background_blur.jpg')");
+
     document.documentElement.style
         .setProperty('--lightGrey', '#fff');
 }
@@ -136,6 +136,7 @@ function changeThemeFog() {
 function changeThemeSun() {
     $('.conent-wrapper').css("background-image", "url('graphics/sun_background.jpg')");
     $('.all-background').css("background-image", "url('graphics/sun_background_blur.jpg')");
+
     document.documentElement.style
         .setProperty('--lightGrey', '#fff');
 }
@@ -143,6 +144,7 @@ function changeThemeSun() {
 function changeThemeCloud() {
     $('.conent-wrapper').css("background-image", "url('graphics/cloud_background.jpg')");
     $('.all-background').css("background-image", "url('graphics/cloud_background_blur.jpg')");
+
     document.documentElement.style
         .setProperty('--lightGrey', '#fff');
 }
